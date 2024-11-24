91761738b596e72d82551d6431a471d4f97abb03
Rafik20012 <77621794+Rafik20012@users.noreply.github.com>
2024-11-24 14:41:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show the Login Modal
+function showLoginModal() {
+  document.getElementById("login-modal").style.display = "flex";
+}
+
+// Close the Login Modal
+function closeLoginModal() {
+  document.getElementById("login-modal").style.display = "none";
+}
